8832b6934535d5140e1c898153a1af09f137b574
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-26 11:18:05 -0700

    Solution

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links.js";
 
-function About() {
+function About(props) {
+  const bioInfo = props.bio;
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioInfo && bioInfo !== "" && 
+      <p>{props.bio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/"/>
     </div>
   );
 }
